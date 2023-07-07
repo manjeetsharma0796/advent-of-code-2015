ad79e5ea46ddfe03d11108fbb11f29db6696f1d8
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-07-07 11:01:52 +0530

    DAY-2: Added message for part1 in main.js

--- day-2/main.js
+++ day-2/main.js
@@ -5,7 +5,7 @@ const main = () => {
   const rawBoxDimensionsSet = fs.readFileSync('./resources/box-dimensions.txt', 'utf-8').split('\n');
   const totalPaperArea = calculateTotalPaperArea(rawBoxDimensionsSet);
 
-  console.log(totalPaperArea);
+  console.log('PART 1: The total wrapper paper area required is :', totalPaperArea);
 }
 
 main();
