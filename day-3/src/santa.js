a311ed3b3b85dd8224f0214ec8812125b47f68c6
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-07-07 15:37:48 +0530

    DAY-3: Added moveUp which moves santa

--- day-3/src/santa.js
+++ day-3/src/santa.js
@@ -2,6 +2,11 @@ class Santa {
   constructor() {
     this.currentCoordinate = { x: 0, y: 0 };
   }
+
+  moveUp() {
+    this.currentCoordinate.y += 1;
+  }
+  
   get status() {
     const currentCoordinate = this.currentCoordinate;
     return currentCoordinate;
--- day-3/test/test.js
+++ day-3/test/test.js
@@ -3,12 +3,37 @@ const { deepStrictEqual } = require('assert');
 const { Santa } = require('../src/santa');
 
 describe('Santa', () => {
-  it('Should not move santa for no direction', () => {
-    const santa = new Santa();
+  describe('status', () => {
+    it('Should give status of santa', () => {
+      const santa = new Santa();
 
-    const actual = santa.status;
-    const expected = { x: 0, y: 0 };
+      const actual = santa.status;
+      const expected = { x: 0, y: 0 };
 
-    deepStrictEqual(actual, expected);
+      deepStrictEqual(actual, expected);
+    });
+  });
+
+  describe('moveUp', () => {
+    it('Should move santa to north direction', () => {
+      const santa = new Santa();
+      santa.moveUp();
+
+      const actual = santa.status;
+      const expected = { x: 0, y: 1 };
+
+      deepStrictEqual(actual, expected);
+    });
+
+    it('Should move santa to north direction twice', () => {
+      const santa = new Santa();
+      santa.moveUp();
+      santa.moveUp();
+
+      const actual = santa.status;
+      const expected = { x: 0, y: 2 };
+
+      deepStrictEqual(actual, expected);
+    });
   });
 });
