cc93b55569300072f85ed6a419b6244d89f5ab5d
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-07-08 18:22:37 +0530

    DAY-3: Santa class refactored

--- day-3/src/santa.js
+++ day-3/src/santa.js
@@ -3,20 +3,26 @@ class Santa {
     this.currentCoordinate = { x: 0, y: 0 };
   }
 
+
+  #move(delta) {
+    this.currentCoordinate.y += delta.y;
+    this.currentCoordinate.x += delta.x;
+  }
+
   moveUp() {
-    this.currentCoordinate.y += 1;
+    this.#move({ x: 0, y: 1 });
   }
 
   moveDown() {
-    this.currentCoordinate.y -= 1;
+    this.#move({ x: 0, y: -1 });
   }
 
   moveRight() {
-    this.currentCoordinate.x += 1;
+    this.#move({ x: 1, y: 0 });
   }
 
   moveLeft() {
-    this.currentCoordinate.x -= 1;
+    this.#move({ x: -1, y: 0 });
   }
 
   get status() {
