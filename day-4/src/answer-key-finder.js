d716a95ed55d6d572776c5f55a602db8c7834b7e
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-07-09 14:51:06 +0530

    DAY-4: Explicit usuage of assert in test files

--- day-4/src/answer-key-finder.js
+++ day-4/src/answer-key-finder.js
@@ -1,5 +1,21 @@
-const findAnswerKey = (secretKey) => {
-  return 0;
+const {createHash} = require('node:crypto')
+
+const findAnswerKey = (secretKey, numberOfZeroesInFrontOfHash) => {
+  let answerKey = 1;
+
+  while(true) {
+    const myHash = createHash('md5');
+    myHash.update(secretKey + answerKey);
+    
+    const hash = (myHash.digest('hex'));
+    const firstFiveHashString = hash
+    .slice(0,numberOfZeroesInFrontOfHash);
+    
+    if(firstFiveHashString === '0'.repeat(numberOfZeroesInFrontOfHash)) {
+      return answerKey;
+    }
+    answerKey++;
+  }
 }
 
 exports.findAnswerKey = findAnswerKey;
--- day-4/test/answer-key-finder-test.js
+++ day-4/test/answer-key-finder-test.js
@@ -1,13 +1,19 @@
 const { describe, it } = require('node:test');
-const { strictEqual } = require('assert');
+const assert = require('assert');
 const { findAnswerKey } = require('../src/answer-key-finder');
 
 describe('findAnswerKey', () => {
-  it('Should be give answer key whose md5 hash has five leading zeroes', () => {
-    const secretKey = 'a';
-    const actual = findAnswerKey(secretKey);
-    const expected = 0;
+  it('Should find answer key whose md5 hash has one leading zeroes', () => {
+    assert.strictEqual(findAnswerKey('a', 1), 27);
+  });
 
-    strictEqual(actual, expected);
-  })
-})
+  it('Should find answer key whose md5 hash has five leading zeroes', () => {
+    assert.strictEqual(findAnswerKey('b', 5), 961275);
+    assert.strictEqual(findAnswerKey('abcdef', 5), 609043);
+    assert.strictEqual(findAnswerKey('yzbqklnj', 5), 282749);
+  });
+
+  it('Should find answer key whose md5 hash has six leading zeroes', () => {
+    assert.strictEqual(findAnswerKey('yzbqklnj', 6), 9962624);
+  });
+});
